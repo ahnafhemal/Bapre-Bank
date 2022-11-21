be94d7e1e55d654f5bede4e7a6cc263accb6a65d
ahnafhemal <hemalhasan140@gmail.com>
2022-11-21 14:08:31 +0600

    six

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,14 @@
 
 //handle deposit button event
 document.getElementById('deposit-button').addEventListener('click', function () {
-    console.log('deposit button click');
+
+    //get the ammount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+
+    //
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+    depositInput.value = ''
 })
