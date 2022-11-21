6c210469676c69056383d4027e6e0581a5d7cca1
ahnafhemal <hemalhasan140@gmail.com>
2022-11-21 15:00:45 +0600

    seven

--- js/banking.js
+++ js/banking.js
@@ -4,11 +4,28 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     //get the ammount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositAmunt = depositInput.value;
 
-    //
+    // addd deposit input value into totaldeposit
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+    const PreviousdepositAmmount = depositTotal.innerText;
+
+    //PreviousdepositAmmount + newdeposit
+    const newdepositTotal = parseFloat(PreviousdepositAmmount) + parseFloat(newDepositAmunt);
+    depositTotal.innerText = newdepositTotal;
+
+    //update account balance
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText) + parseFloat(newDepositAmunt);
+
+    balanceTotal.innerText = previousBalanceTotal;
+
+    // clear thd depsoit
     depositInput.value = ''
 })
+
+
+
+
